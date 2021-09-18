cd5cf2ef039b15c66b79eecc376fee382eb208ac
shahreaz <ashahreaz@gmail.com>
2021-09-18 23:59:39 +0600

    User register route

--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,53 @@
 const router = require("express").Router();
 const Joi = require("joi");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+
+// models
+const User = require("../models/User");
 
 // request body validation schema
 const schema = Joi.object({
 	name: Joi.string().min(3).max(30).required(),
 	password: Joi.string()
 		.pattern(new RegExp("^[a-zA-Z0-9]{6,30}$"))
+		.min(6)
 		.required(),
 	email: Joi.string().email().required(),
 });
 
 // route POST api/users
-// desc Test route
+// desc create user
 // access Public
-router.post("/", (req, res) => {
-	const body = schema.validate(req.body);
+router.post("/", async (req, res) => {
+	const { error, value } = schema.validate(req.body);
+	if (error) return res.status(400).send(error);
 
-	if (body?.error) res.send(body.error);
-	res.send(body.value);
+	try {
+		// user exist or not
+		const { name, email, password } = value;
+		let user = await User.findOne({ email });
+		if (user) {
+			return res.status(400).json({
+				details: [{ message: "User already exists" }],
+			});
+		}
+
+		// gravatar url generate
+		const avatar = gravatar.url(email, { s: "200", r: "pg", d: "mm" });
+		// create user instance
+		user = new User({ name, email, avatar, password });
+		// hash password
+		const salt = await bcrypt.genSalt(10);
+		user.password = await bcrypt.hash(password, salt);
+		// save user to the db
+		await user.save();
+
+		res.send(user);
+	} catch (error) {
+		console.log(error.message);
+		res.status(500).send("Server error");
+	}
 });
 
 module.exports = router;
