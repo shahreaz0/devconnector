92e083cad540abb9200aea3a5f156d521e11c09b
shahreaz <ashahreaz@gmail.com>
2021-09-19 16:52:16 +0600

    Current user profile route

--- routes/profile.js
+++ routes/profile.js
@@ -1,10 +1,32 @@
 const router = require("express").Router();
 
-// route api/profile
-// desc Test route
-// access Public
-router.get("/", (req, res) => {
-	res.send("Profile Routes");
+// models
+const Profile = require("../models/Profile");
+
+// middleware
+const auth = require("../middlewares/auth");
+
+// route 	GET api/profile/me
+// desc 	Get current user
+// access 	Private
+router.get("/me", auth, async (req, res) => {
+	try {
+		const profile = await Profile.findOne({ user: req.user.id }).populate(
+			"user",
+			["name", "avatar"]
+		);
+
+		if (!profile) {
+			return res
+				.status(400)
+				.send({ message: "There is no profile for this user" });
+		}
+
+		res.send(profile);
+	} catch (error) {
+		console.log(error);
+		res.status(500).send("Server Error.");
+	}
 });
 
 module.exports = router;
